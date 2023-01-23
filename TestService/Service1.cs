d6ce69ca31a1bced9c3dc431df2b3ce4ae321477
Cem ASLAN <cemaslan@hotmail.fr>
2023-01-23 14:14:58 +0300

    Add project files.

---
+++ TestService/Service1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private delegate int KeyboardHookProc(int code, int wParam, ref KeyboardHookStruct lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardHookProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(IntPtr idHook, int nCode, int wParam, ref KeyboardHookStruct lParam);
+
+        public   IntPtr _hook;
+
+        private struct KeyboardHookStruct
+        {
+            public int VkCode;
+            public int ScanCode;
+            public int Flags;
+            public int Time;
+            public int DwExtraInfo;
+        }
+        public Service1()
+        {
+            InitializeComponent();
+            var hInstance = LoadLibrary("User32");
+            _hook = SetWindowsHookEx(13, HookProc, hInstance, 0);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                Logla("TEST", "cem" + Environment.NewLine);
+                var hInstance = LoadLibrary("User32");
+                _hook = SetWindowsHookEx(13, HookProc, hInstance, 0);
+            }
+            catch (Exception)
+            {
+                 
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+        Keys _OncekiKey;
+        private int HookProc(int code, int wParam, ref KeyboardHookStruct lParam)
+        {
+            if (code >= 0 && lParam.Flags == 0)
+            {
+                var key = (Keys)lParam.VkCode;
+                //textBox1.Text += key.ToString() + "" + Environment.NewLine;
+                Logla("TEST", key.ToString() + Environment.NewLine);
+                if (key == Keys.B && _OncekiKey == Keys.LControlKey)
+                {
+                    SendKeys.Send("Hasan Bey");
+
+                }
+                _OncekiKey = key;
+            }
+            return CallNextHookEx(_hook, code, wParam, ref lParam);
+        }
+        private void Logla(string cinsi, string kayit)
+        {
+            try
+            {
+                if (!Directory.Exists("Loglar")) Directory.CreateDirectory("Loglar");
+                var dosyaAdi = ($"{Application.StartupPath}\\Loglar\\{cinsi} - {DateTime.Now.ToString("yyyyMMdd")}.txt");
+                using (var yazici = new StreamWriter(dosyaAdi, true))
+                {
+                    yazici.Write($"{Environment.NewLine}{kayit}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Loglayıcı bile hata verdi:{ex.ToString()}");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp35/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp35
+{
+    public partial class Form1 : Form
+    {
+        private delegate int KeyboardHookProc(int code, int wParam, ref KeyboardHookStruct lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardHookProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(IntPtr idHook, int nCode, int wParam, ref KeyboardHookStruct lParam);
+
+        private readonly IntPtr _hook;
+
+        private struct KeyboardHookStruct
+        {
+            public int VkCode;
+            public int ScanCode;
+            public int Flags;
+            public int Time;
+            public int DwExtraInfo;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            var hInstance = LoadLibrary("User32");
+            _hook = SetWindowsHookEx(13, HookProc, hInstance, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Keys _OncekiKey;
+        private int HookProc(int code, int wParam, ref KeyboardHookStruct lParam)
+        {
+            if (code >= 0 && lParam.Flags == 0)
+            {
+                var key = (Keys)lParam.VkCode;
+                    textBox1.Text += key.ToString() + "" + Environment.NewLine;
+
+                if (key == Keys.B&& _OncekiKey==Keys.LControlKey)
+                {
+                    SendKeys.Send("Hasan Bey");
+
+                }
+                if (key == Keys.Q )
+                {
+                    SendKeys.Send("cem Bey");
+
+                }
+                _OncekiKey = key;
+            }
+            return CallNextHookEx(_hook, code, wParam, ref lParam);
+        }
+    }
+}
